b11acc28f8871aa2c02567ccd08c136883750135
pimmatc <122541111+pimmatc@users.noreply.github.com>
2024-07-09 00:12:03 +0700

    index.js

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,11 @@ import { createRouter, createWebHistory } from 'vue-router'
 import SignIn from '../components/SignIn.vue'
 import SignUp from '../components/SignUp.vue'
 
+// bam added
+import News from '../views/NewsView.vue'
+import Profile from '../views/ProfileView.vue'
+import Edit from '../views/EditProfile.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -18,7 +23,22 @@ const router = createRouter({
       path: '/signup',
       name: 'SignUp',
       component: SignUp
-    }
+    },
+    {
+      path: '/news',
+      name: 'News',
+      component: News
+    },
+    {
+      path: '/profile',
+      name: 'Profile',
+      component: Profile
+    },
+    {
+      path: '/edit',
+      name: 'Edit',
+      component: Edit
+    },
   ]
 })
 
